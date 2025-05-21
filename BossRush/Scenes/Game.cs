c0435bf23836b635573ea150132fdfa10199dc22
Nomeos <mathieu.rabot01@gmail.com>
2025-05-21 15:09:29 +0200

    update

--- BossRush/Scenes/Game.cs
+++ BossRush/Scenes/Game.cs
@@ -1,9 +1,11 @@
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace BossRush.Scenes;
 
 public class Game(GameManager gm, SceneManager sm) : Scene(gm, sm)
 {
+    
     protected override void Load()
     {
         //throw new System.NotImplementedException();
@@ -11,6 +13,7 @@ public class Game(GameManager gm, SceneManager sm) : Scene(gm, sm)
 
     protected override void Draw()
     {
+        Globals.SpriteBatch.Draw(Globals.WhitePixel, new Rectangle(0, 0, (int)Globals.ScreenSize.X, (int)Globals.ScreenSize.Y), new Color(160, 200, 120));
         //throw new System.NotImplementedException();
     }
 
@@ -21,6 +24,6 @@ public class Game(GameManager gm, SceneManager sm) : Scene(gm, sm)
 
     public override void Activate()
     {
-        //throw new System.NotImplementedException();
+        Load();
     }
 }
